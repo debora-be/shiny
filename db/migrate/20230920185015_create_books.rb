eb8bd8e401f5450a3a2329e27b6c3b086196bfb8
debora berte <arobedbee@gmail.com>
2023-09-20 15:53:51 -0300

    Create :authors and :books contexts

---
+++ db/migrate/20230920185015_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.date :published_date
+      t.references :author, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_184552) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_185015) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.text "bio"
